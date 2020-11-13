99820cb6aeb5c4aa6bcefefd48f4f2a8e2cfaa35
jelena viskovic <jelena.viskovic@gmail.com>
2020-11-13 14:58:56 +0000

    transform game objects

--- Assets/Script/TransformGamesObject.cs
+++ Assets/Script/TransformGamesObject.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class TransformGamesObject : MonoBehaviour
 {
+
+    public GameObject replacementObject;
+
+    public float transformationTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        replacementObject.SetActive(false);
+
+        StartCoroutine(TransformationCountdown());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public IEnumerator TransformationCountdown()
+    {
+        yield return new WaitForSeconds(transformationTime);
+
+        replacementObject.SetActive(true);
+
+        this.gameObject.SetActive(false);
+
     }
 }
